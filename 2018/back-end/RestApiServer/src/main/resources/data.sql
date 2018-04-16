fa2dedba340e341b60b77f0a07b5a535f772307e
高橋弘 <htakahashi77786@gmail.com>
2018-04-16 21:34:33 +0900

    SpringBoot実行時、H2DBに自動的にデータをロードできるようにschema.sql,data.sqlという命名のファイルを追加。

---
+++ 2018/back-end/RestApiServer/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO company(companyId,companyName,jobCategoryLevel1, jobCategoryLevel2, registDate, registUser, updateDate, updateUser) values (1, 'A社', '01', '01', DATE'2018-01-01', 'U001', DATE'2018-01-01', 'U001');
+INSERT INTO company(companyId,companyName,jobCategoryLevel1, jobCategoryLevel2, registDate, registUser, updateDate, updateUser) values (1, 'B社', '01', '02', DATE'2018-01-01', 'U001', DATE'2018-01-01', 'U001');
+INSERT INTO company(companyId,companyName,jobCategoryLevel1, jobCategoryLevel2, registDate, registUser, updateDate, updateUser) values (1, 'C社', '01', '03', DATE'2018-01-01', 'U001', DATE'2018-01-01', 'U001');
+INSERT INTO company(companyId,companyName,jobCategoryLevel1, jobCategoryLevel2, registDate, registUser, updateDate, updateUser) values (1, 'D社', '02', '01', DATE'2018-01-01', 'U001', DATE'2018-01-01', 'U001');
+
